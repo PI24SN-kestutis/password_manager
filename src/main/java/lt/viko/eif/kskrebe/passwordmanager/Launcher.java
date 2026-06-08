8f55657bb8bd651d734b0469dcb95b55e8a0312c
Kęstutis Skrebė <kestutis.skrebe@stud.viko.lt>
2026-06-08 13:12:57 +0300

    add Launcher class and integrate CryptoService for encryption demonstration

---
+++ src/main/java/lt/viko/eif/kskrebe/passwordmanager/Launcher.java
@@ -0,0 +1,10 @@
+package lt.viko.eif.kskrebe.passwordmanager;
+
+public class Launcher {
+
+
+    //Nuo Java 11+ tai dažnai išsprendžia JavaFX startavimo problemas.
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
--- src/main/java/lt/viko/eif/kskrebe/passwordmanager/Main.java
+++ src/main/java/lt/viko/eif/kskrebe/passwordmanager/Main.java
@@ -2,14 +2,37 @@ package lt.viko.eif.kskrebe.passwordmanager;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import lt.viko.eif.kskrebe.passwordmanager.service.CryptoService;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) {
+
+        try {
+
+            CryptoService crypto =
+                    new CryptoService();
+
+            String encrypted =
+                    crypto.encrypt("Labas123");
+
+            String decrypted =
+                    crypto.decrypt(encrypted);
+
+            System.out.println("Encrypted:");
+            System.out.println(encrypted);
+
+            System.out.println();
+
+            System.out.println("Decrypted:");
+            System.out.println(decrypted);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         stage.setTitle("Slaptažodžių tvarkyklė");
-        stage.setWidth(900);
-        stage.setHeight(600);
         stage.show();
     }
 
